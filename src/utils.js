b0c6345398edff2cebc5bf715efc3c1ebc28efa2
Sasha Yeremeev <c3pions@gmail.com>
2017-07-08 12:41:59 +0300

    edit

--- src/utils.js
+++ src/utils.js
@@ -166,7 +166,7 @@ const arrayElementFromTheEnd = function(arr){
  * @param {Function} callback
  * @return {Array} arr
  */
-  const arraySorting = function BubbleSort(arr){
+  const arraySorting = function Sort(arr){
     for (let i = 0; i < arr.length-1; i++) { 
         for (let j = 0; j < arr.length-1-i; j++) { 
              if (arr[j+1] < arr[j]) { 
